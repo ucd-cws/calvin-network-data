447f9f8716d6e5e88936e5cf6a7f0cb90f0ca2fd
Quinn Hart <qjhart@ucdavis.edu>
2015-08-07 12:30:40 -0700

    Added beginning of network inspector

---
+++ bin/inspect/index.js
@@ -0,0 +1,67 @@
+"use strict";
+var netfs = require('./lib/networkfs')
+  , create = require('./lib/create')
+  , Getopt = require('node-getopt')
+  , SphericalMercator = require('sphericalmercator')
+  , sm , ll , pt , delta
+  , getopt , opt
+  , items;
+
+getopt = new Getopt([
+ ['d' , 'dir=ARG'],
+ ['l' , 'll=ARG' , 'longitude-latitude pair'],
+ [''  , 'delta=ARG', 'delta in (m) from -ll to locate pt'],
+ ['r' , 'reverse', 'reverse -ll order (latitude-longitude' ],
+ ['h' , 'help'],
+]).bindHelp();
+
+opt = getopt.parse(process.argv.slice(2));
+console.info(opt);
+
+if (opt.options.dir) {
+    try {
+	process.chdir(opt.options.dir);
+    }
+    catch (err) {
+	console.log('Cannot chdir to '+opt.options.dir+' '+ err);
+    }
+}
+
+if (opt.options.ll) {
+    sm = new SphericalMercator ( { size:256 } );
+    ll=opt.options.ll.split(',');
+    if ( ll.length !== 2) {
+	console.error('-ll '+opt.options.ll+' malformed.');
+	process.exit(1);
+    }
+    if (opt.options.reverse) { ll.reverse(); }
+    if (opt.options.delta) {
+	delta=opt.options.delta.split(',');
+	if ( delta.length !== 2) {
+	    console.error('--delta '+opt.options.delta+' malformed.');
+	    process.exit(1);
+	}
+	pt = sm.forward(ll);
+	pt[0] += Number(delta[0]);
+	pt[1] += Number(delta[1]);
+	ll = sm.inverse(pt);
+    }
+}
+
+netfs.fetch('.',function(items){
+  console.log('Found '+items.length+' nodes and links.  Creating pri files...');
+});
+
+if (opt.argv) {
+ netfs.each(opt.argv,function(item) {
+     var json=netfs.geojson(item);
+     console.info(item,json);
+});
+}
+//netfs.list('.',function(items){
+//    for (var i = 0; i < items.length; i++) {
+//     console.log(items[i]);
+//    }
+//});
+
+
---
+++ bin/inspect/lib/networkfs.js
@@ -0,0 +1,123 @@
+{
+"use strict";
+
+var walk = require('walk')
+  , fs = require('fs')
+  , path = require('path')
+  ;
+
+function fetch (dir, callback) {
+  var items = [];
+
+  walker = walk.walk(dir, {});
+
+  walker.on("file", function (root,fileStats, next) {
+   var json = importGeojson(root+'/'+fileStats.name);
+   if( json ) items.push(json);
+   next();
+  });
+
+  walker.on("errors", function (root, nodeStatsArray, next) {
+   next();
+  });
+
+  walker.on("end", function () {
+   callback(items);
+  });
+};
+
+function each(items,callback) {
+  for (var i=0; i<items.length; i++) {
+      callback(items[i]);
+  }
+}
+
+function list (dir, callback) {
+  var items = []
+    , file
+    , walker = walk.walk(dir, {});
+
+  walker.on("file", function (root,fileStats, next) {
+      file=fileStats.name;
+   if (file.match(/\.geojson$/)) {
+       items.push(root);
+   }
+   next();
+  });
+
+  walker.on("errors", function (root, nodeStatsArray, next) {
+   next();
+  });
+
+  walker.on("end", function () {
+   callback(items);
+  });
+};
+
+function importGeojson(file) {
+  if( !file.match(/\.geojson$/) ) return null;
+
+  var json;
+  try {
+    // currently some badness in the JSON
+    json = fs.readFileSync(file, 'utf-8').replace(/[\r\n]/g, '');
+    json = JSON.parse(json);
+  } catch(e) {
+    console.log('Invalid JSON: '+file);
+    return null;
+  }
+
+  if( !json ) return null;
+  if( !json.properties ) return null;
+  if( !json.properties.type || json.properties.type == 'Region' ) return null;
+
+  json.properties.repoFile = file;
+
+  return json;
+}
+
+
+function geojson(node) {
+    var json
+      , files=['index','region','node','link']
+      , file;
+    for (var i=0; i<files.length;i++) {
+	try {
+	    file=files[i]+".geojson";
+	    json=fs.readFileSync(path.join(node,file),'utf-8');
+	    console.log("Found",file);
+	    break;
+	} catch(e) {
+	    if (e.errno !== -2) {
+		console.log(e);
+	    } else {
+		if (i === files.length-1) {
+		    console.log('Node not found')
+		}
+	    }
+	}
+    }
+  try {
+    // currently some badness in the JSON
+    json.replace(/[\r\n]/g, '');
+    json = JSON.parse(json);
+  } catch(e) {
+    console.log('Invalid JSON: '+node);
+    return null;
+  }
+
+  if( !json ) return null;
+  if( !json.properties ) return null;
+  if( !json.properties.type || json.properties.type == 'Region' ) return null;
+
+  json.properties.repoFile = file;
+
+  return json;
+}
+
+
+exports.fetch=fetch;
+exports.list=list;
+exports.each=each;
+exports.geojson=geojson;
+};
